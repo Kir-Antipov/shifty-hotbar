e01b17b65ca90d2a46f7490da62337f4a9c6c5f0
Kir_Antipov <kp.antipov@gmail.com>
2022-06-14 17:19:31 +0300

    Implemented client-side wrapper over `PlayerInventory`
    ... that simplifies slot swapping

---
+++ src/main/java/dev/kir/shiftyhotbar/util/SlotSwapper.java
@@ -0,0 +1,39 @@
+package dev.kir.shiftyhotbar.util;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.slot.SlotActionType;
+
+import java.util.Optional;
+
+@Environment(EnvType.CLIENT)
+public final class SlotSwapper {
+    private final ClientPlayerInteractionManager interactionManager;
+    private final PlayerEntity player;
+    private final int syncId;
+
+    private SlotSwapper(ClientPlayerInteractionManager interactionManager, PlayerEntity player, int syncId) {
+        this.interactionManager = interactionManager;
+        this.player = player;
+        this.syncId = syncId;
+    }
+
+    public void swap(int left, int right) {
+        this.interactionManager.clickSlot(this.syncId, left, right, SlotActionType.SWAP, this.player);
+    }
+
+    public static Optional<SlotSwapper> from(PlayerInventory inventory) {
+        return SlotSwapper.from(MinecraftClient.getInstance(), inventory);
+    }
+
+    public static Optional<SlotSwapper> from(MinecraftClient client, PlayerInventory inventory) {
+        if (client.interactionManager == null || inventory == null || inventory.player == null) {
+            return Optional.empty();
+        }
+        return Optional.of(new SlotSwapper(client.interactionManager, inventory.player, inventory.player.playerScreenHandler.syncId));
+    }
+}
